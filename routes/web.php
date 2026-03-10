6356dee85f4aa1cb032c3d9c7fdf411a78c5fcd6
Sourov Biswas <sourovcodes@gmail.com>
2026-03-10 08:07:28 +0600

    feat: Update homepage route to redirect to dashboard and adjust related test

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\InvitationAcceptController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/dashboard');
 
 Route::get('/invitations/accept/{token}', InvitationAcceptController::class)
     ->name('invitation.accept');
